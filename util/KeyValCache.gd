c68760dd0e20649a8b39c1e6d4482c89a8aa006f
Adam Nash <adam@smr.llc>
2024-01-15 12:05:16 -0500

    store jam launch user data/settings globally vs per-project

--- util/KeyValCache.gd
+++ util/KeyValCache.gd
@@ -1,7 +1,8 @@
 extends RefCounted
 class_name KeyValCache
 
-var cache_path: String = "user://jamlaunchcache.json"
+var old_default_cache_path: String = "user://jamlaunchcache.json"
+var cache_path: String = OS.get_data_dir().path_join("jam-launch/data.json")
 
 func store(key: String, val: String):
 	var c := get_cache()
@@ -47,23 +48,41 @@ class CacheResult:
 		r.cache = data
 		return r
 
-func get_cache() -> CacheResult:
-	if !FileAccess.file_exists(cache_path):
-		var e = CacheResult.err("cache file '%s' does not exist" % cache_path)
+func get_cache(path_override: String = "") -> CacheResult:
+	var path = cache_path
+	if not path_override.is_empty():
+		path = path_override
+	if !FileAccess.file_exists(path):
+		if FileAccess.file_exists(old_default_cache_path) and path_override.is_empty():
+			var res = get_cache(old_default_cache_path)
+			if not res.errored:
+				write_cache(res.cache, cache_path)
+				return res
+		var e = CacheResult.err("cache file '%s' does not exist" % path)
 		e.no_file = true
 		return e
-	var cache_string := FileAccess.get_file_as_string(cache_path)
+	var cache_string := FileAccess.get_file_as_string(path)
 	var data = JSON.parse_string(cache_string)
 	if data == null:
-		return CacheResult.err("failed to parse cache file '%s' as JSON" % cache_path)
+		return CacheResult.err("failed to parse cache file '%s' as JSON" % path)
 	return CacheResult.result(data as Dictionary)
 
-func write_cache(cache: Dictionary):
+func write_cache(cache: Dictionary, path_override: String = ""):
+	if path_override.is_empty():
+		path_override = cache_path
+		
 	var s := JSON.stringify(cache)
 	
-	var f := FileAccess.open(cache_path, FileAccess.WRITE)
+	var base_dir = path_override.get_base_dir()
+	if not FileAccess.file_exists(base_dir):
+		var err = DirAccess.make_dir_recursive_absolute(base_dir)
+		if err != OK:
+			printerr("failed to create directories for cache file '%s' - err %d" % [path_override, err])
+			return false
+	
+	var f := FileAccess.open(path_override, FileAccess.WRITE)
 	if f == null:
-		printerr("Failed to open jam launch cache for writing")
+		printerr("failed to open cache for writing at '%s'" % path_override)
 		return false
 	f.store_string(s)
 	f.close()
